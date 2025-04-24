33b0ae4281e31ac3e9acad9076125c5dfae7fcf2
phamhieu1222 <hieu712202@gmail.com>
2025-04-24 14:18:23 +0700

    first commit

---
+++ src/SelectionSort.java
@@ -0,0 +1,29 @@
+public class SelectionSort {
+    static double[] list = {1, 9, 4.5, 6.6, 5.7, -4.5};
+    public static void selectionSort(double[] a) {
+        for (int i = 0; i < list.length - 1; i++) {
+            int minIndex = i;
+            double currentMin = list[i];
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    minIndex = j;
+
+                }
+            }if(minIndex!=1){
+             list[minIndex] = list[i];
+             list[i] = currentMin;
+
+
+            }
+        }
+    }
+public static void main(String[] args){
+        selectionSort(list);
+        for (int i = 0; i < list.length - 1; i++) {
+            System.out.println(list[i]);
+            
+        }
+}
+
+}
